ae9f7870d379633f876c3197066f2a1a7ff1affb
Danilo Tanaka <tanakadanilo867@gmail.com>
2023-02-20 19:51:55 -0300

    adicionando pacotes - primeng

--- marketplace_servicos/src/app/app.module.ts
+++ marketplace_servicos/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+import {MenuItem} from 'primeng/api';                  //api
+
 @NgModule({
   declarations: [
     AppComponent
