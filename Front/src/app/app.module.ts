6b21347c386ddcaa46ea8681414b36ab564d7607
Danilo Tanaka <tanakadanilo867@gmail.com>
2023-04-10 19:13:04 -0300

    modificando projeto para usar nebular no lugar de primeng

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -5,9 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './exports/menu/menu.component';
 
-import { InputTextModule } from 'primeng/inputtext';
-import {ButtonModule} from 'primeng/button';
 import { FooterComponent } from './exports/footer/footer.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbAuthModule } from '@nebular/auth';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { NbButtonModule } from '@nebular/theme';
+import { NbInputModule } from '@nebular/theme';
 
 @NgModule({
   declarations: [
@@ -18,8 +23,14 @@ import { FooterComponent } from './exports/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    InputTextModule,
-    ButtonModule
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot(),
+    NbLayoutModule,
+    NbAuthModule.forRoot(),
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbEvaIconsModule,
+    NbButtonModule,
+    NbInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
