4d7491a840890348719dcb950af376383fa5edb4
tanakadanilo <tanakadanilo867@gmail.com>
2023-05-15 20:00:49 -0300

    correção - liberação de acesso

--- Back/src/main/java/com/devnari/contrataai/model/User.java
+++ Back/src/main/java/com/devnari/contrataai/model/User.java
@@ -1,65 +1,57 @@
 package com.devnari.contrataai.model;
 
 import java.util.Collection;
-import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "users")
 public class User implements UserDetails {
-
-	/**
-	 * 
-	 */
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue
-	private int id;
-
-	@Column(unique = true)
-	private String username;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 	private String password;
+	private String username;
+	private String login;
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
 	public boolean isAccountNonExpired() {
 		// TODO Auto-generated method stub
-		return true;
+		return false;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
 		// TODO Auto-generated method stub
-		return true;
+		return false;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
 		// TODO Auto-generated method stub
-		return true;
+		return false;
 	}
 
 	@Override
 	public boolean isEnabled() {
 		// TODO Auto-generated method stub
-		return true;
+		return false;
 	}
 
 }
