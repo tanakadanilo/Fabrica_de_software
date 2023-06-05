505355f69298a6adc846e7e997d97865668238b9
Danilo Tanaka Vieira Marques <danilo.marques@agr.go.gov.br>
2023-06-05 11:46:35 -0300

    correção cors

--- Back/src/main/java/com/devnari/contrataai/configurations/SecurityConfiguration.java
+++ Back/src/main/java/com/devnari/contrataai/configurations/SecurityConfiguration.java
@@ -2,13 +2,15 @@ package com.devnari.contrataai.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.devnari.contrataai.services.UsuarioLoggadoService;
 
@@ -16,26 +18,38 @@ import com.devnari.contrataai.services.UsuarioLoggadoService;
 public class SecurityConfiguration {
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		return http.authorizeRequests().anyRequest().permitAll().and().build();
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http.cors().disable().authorizeHttpRequests().anyRequest().permitAll().and().build();
 	}
 
 	@Bean
-	public UserDetailsService userDetailsService() {
+	UserDetailsService userDetailsService() {
 		return new UsuarioLoggadoService();
 	}
 
 	@Bean
-	public PasswordEncoder passwordEncoder() {
+	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
 	@Bean
-	public WebSecurityCustomizer webSecurityCustomizer() {
+	WebSecurityCustomizer webSecurityCustomizer() {
 		return (web) -> web.ignoring().anyRequest();
 	}
 
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.addAllowedOrigin("*");
+		configuration.addAllowedMethod("*");
+		configuration.addAllowedHeader("*");
+		configuration.setAllowCredentials(true);
 
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+
+		return source;
+	}
 
 //	@Bean
 //	CorsConfigurationSource corsConfigurationSource() {
