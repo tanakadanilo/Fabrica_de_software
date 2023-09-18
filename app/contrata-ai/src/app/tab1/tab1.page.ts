30f12f04973fac7ff82aaa9d005caa8e3d6fb064
danilo.marques <tanakadanilo867@gmail.com>
2023-09-17 22:09:48 -0300

    criando passaro voando (sem falas, por enquanto)

--- app/contrata-ai/src/app/tab1/tab1.page.ts
+++ app/contrata-ai/src/app/tab1/tab1.page.ts
@@ -1,12 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  initialX: number = 0;
+  initialY: number = 0;
 
-  constructor() {}
-
+  // Função para atualizar a posição da imagem
+  public atualizarPosicao(event: MouseEvent) {
+    // Atualize as coordenadas com base na posição do mouse
+    this.initialX = event.clientX;
+    this.initialY = event.clientY;
+  }
 }
--- app/contrata-ai/src/app/tab2/tab2.page.ts
+++ app/contrata-ai/src/app/tab2/tab2.page.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  contratante: any = {
+    nome: 'Unknown User',
+    contato: {
+      contato: 'Unknown',
+      email: 'Unknown@Unknown.com',
+      telefone: 'Unknown',
+    },
+    cpf: 'Unknown',
+    endereco: {
+      cep: 'Unknown',
+      logradouro: 'Unknown',
+      numero: 'Unknown',
+      cidade: 'Unknown',
+      quadra: 'Unknown',
+      lote: 'Unknown',
+      uf: 'Unknown',
+      bairro: 'Unknown',
+      complemento: 'Unknown',
+    },
+    foto: 'Unknown.Unknown',
+    usuario: 'Unknown User',
+  };
+  ufs: any;
+  ufSelecionada: any = '';
+  cities: any;
+  constructor(private http: HttpClient) {}
 
-  constructor() {}
-
+  ngOnInit() {
+    this.http.get('http://localhost:8080/contratante').subscribe((a: any) => {
+      this.contratante = a.data.content[1];
+      console.log(this.contratante);
+    });
+  }
 }
