64c397455de2976c3ac580bea7725c3b9f609fee
danilo.marques <tanakadanilo867@gmail.com>
2023-10-03 22:10:40 -0300

    criando interfaces de response e paginavel

---
+++ Front/src/app/exports/interface/paginavel.ts
@@ -0,0 +1,8 @@
+export interface Paginavel<T> {
+  content: T[]; // Conteúdo da página
+  totalElements: number; // Total de elementos na coleção
+  totalPages: number; // Total de páginas
+  number: number; // Número da página atual (baseado em zero)
+  size: number; // Tamanho da página
+  numberOfElements: number; // Número de elementos na página atual
+}
